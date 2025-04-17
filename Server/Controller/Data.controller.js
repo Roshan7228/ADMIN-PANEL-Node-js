b867525bd1c852ddcc19106f35ed7d19a4dd97a0
Roshan7228 <7930roshankumar@gmail.com>
2025-04-17 12:50:38 +0530

    Back-End

--- Server/Controller/Data.controller.js
+++ Server/Controller/Data.controller.js
@@ -1,5 +1,6 @@
 let fs = require('fs');
 let { addID } = require('../middlewares/addID.middleware');
+const { json } = require('stream/consumers');
 
 let createdata = (request, response) => {
 
@@ -15,12 +16,12 @@ let createdata = (request, response) => {
         let parsedData = JSON.parse(data);
         let heroes = parsedData.heroes;
 
-        heroes.push(newdata);               // data push first
-        let updatedHeroes = addID(heroes);  // id assign after push
+        heroes.push(newdata);
+        let updatedHeroes = addID(heroes);
 
         parsedData.heroes = updatedHeroes;
 
-        fs.writeFile("./db.json", JSON.stringify(parsedData, null, 2), (err) => {
+        fs.writeFile("./db.json", JSON.stringify(parsedData), (err) => {
             if (err) {
                 return response.status(400).json({
                     message: err.message
@@ -35,4 +36,102 @@ let createdata = (request, response) => {
     });
 };
 
-module.exports = { createdata };
+let gethero = (request, response) => {
+    fs.readFile("./db.json", "utf-8", (error, data) => {
+        if (error) {
+            return response.status(400).json({
+                message: error.message
+            })
+        }
+        let Hero = JSON.parse(data);
+        return response.status(200).json({
+            message: "Get All Hero",
+            Hero
+        })
+    })
+}
+let UpdateVillain = (request, response) => {
+    let { hero_id } = request.params;
+    let { name, health } = request.body;
+
+    if (!name || !health) {
+        return response.status(400).json({
+            message: "Please provide both name and health"
+        });
+    }
+
+    fs.readFile('./db.json', 'utf-8', (err, data) => {
+        if (err) {
+            return response.status(500).json({
+                message: err.message
+            });
+        }
+
+        let Maindata = JSON.parse(data);
+        let heroes = Maindata.heroes;
+
+        let hero = heroes.find(hero => hero.id == hero_id);
+
+        if (!hero) {
+            return response.status(404).json({
+                message: "Hero not found"
+            });
+        }
+        if (hero.villains && hero.villains.length > 0) {
+            hero.villains[0].name = name;
+            hero.villains[0].health = health;
+        } else {
+            hero.villains = [{ name, health }];
+        }
+
+        fs.writeFile('./db.json', JSON.stringify(Maindata, null, 2), 'utf-8', (err) => {
+            if (err) {
+                return response.status(500).json({
+                    message: err.message
+                });
+            }
+            return response.status(200).json({
+                message: "Villain name & health updated successfully!"
+            });
+        });
+    });
+}
+
+let deleteheros = (request, response) => {
+    let { hero_id } = request.params;
+
+    try {
+
+        fs.readFile("./db.json", 'utf-8', (err, data) => {
+            if (err) {
+                message: err.message
+            }
+            let Maindata = JSON.parse(data);
+            let heroes = Maindata.heroes;
+
+            let updateHeroes = heroes.filter((hero) => hero.id != hero_id);
+            fs.writeFile("./db.json", JSON.stringify(updateHeroes), "utf-8", (err) => {
+                if (err) {
+                    return response.status(400).json({
+                        message: err.message
+                    })
+                }
+                response.status(200).json({
+                    message: "Delelte Successfull"
+                })
+            })
+
+
+        })
+
+    } catch (error) {
+        return response.status(400).json({
+            message: error.message
+        })
+    }
+
+
+}
+
+
+module.exports = { createdata, gethero, UpdateVillain, deleteheros };
--- Server/Routes/Data.routes.js
+++ Server/Routes/Data.routes.js
@@ -1,13 +1,16 @@
-let express =require("express");
-const { createdata } = require("../Controller/Data.controller");
+let express = require("express");
+const { createdata, gethero, UpdateVillain, deleteheros } = require("../Controller/Data.controller");
 const { auth } = require("../middlewares/auth.middleware");
 const { logger } = require("../middlewares/logger.middleware");
 
 
-let DataRoutes=express.Router();
+let DataRoutes = express.Router();
 
-DataRoutes.post("/add/hero",auth,logger,createdata);
+DataRoutes.post("/add/hero", auth, logger, createdata);
+DataRoutes.get("/hero", auth, gethero);
+DataRoutes.patch("/update/villain/:hero_id", auth, logger, UpdateVillain);
+DataRoutes.delete("/delete/hero/:hero_id", auth, logger, deleteheros);
 
 
-module.exports=DataRoutes
+module.exports = DataRoutes
 
